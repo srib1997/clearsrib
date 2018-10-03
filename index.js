e6147675c5b1e3481b4c5e6b78068106aa012a6e
srib1997 <peterf1a08@yahoo.com.hk>
2018-10-03 15:18:49 +0800

    added image, correct tittle, 自我介紹

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello comus')
-})
-
-app.get('/about', (req, res) => {
-  res.send('hello srib')
-})
+app.use(express.static('public'))
 
 app.listen(port, () => console.log(`example app listening on port ${port}!`))
